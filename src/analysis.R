623790eddfc73c975967f6330faebc51b7baa379
“Marc <m.birkett2@newcastle.ac.uk>
2021-11-11 18:41:49 +0000

    Made a start

---
+++ src/analysis.R
@@ -0,0 +1,17 @@
+library('ProjectTemplate'); 
+load.project()
+
+#load some libraries
+library(dplyr)
+library(rmarkdown)
+library(ggplot2)
+
+#Import 
+
+#Tidy - explore and sort out with dplyr
+
+#(Visualise > Model > Transform) 
+
+#Understand 
+
+#Communicate
